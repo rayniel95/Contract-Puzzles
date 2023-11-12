2291263d9e82d53185a4d1ce62ae0c97f1e12390
rayniel95 <rainyel.ramos@gmail.com>
2023-11-12 15:50:02 -0500

    solve 4

--- test/game4Test.js
+++ test/game4Test.js
@@ -1,5 +1,6 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
+const { ethers } = require('hardhat');
 
 describe('Game4', function () {
   async function deployContractAndSetVariables() {
@@ -10,10 +11,12 @@ describe('Game4', function () {
   }
   it('should be a winner', async function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
-
+    const [signer0,signer1] = await ethers.getSigners()
     // nested mappings are rough :}
+    await game.connect(signer1).write(await signer0.getAddress())
 
-    await game.win();
+
+    await game.win(await signer1.getAddress());
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
